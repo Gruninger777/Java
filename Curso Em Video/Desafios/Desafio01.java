ce204f6817499feb7e8d23bbdd99c34507052b32
Gruninger777 <fritzgruningerneto@gmail.com>
2025-08-07 23:35:59 -0300

    Add files via upload

---
+++ Curso Em Video/Desafios/Desafio01.java
@@ -0,0 +1,13 @@
+
+package desafio01;
+
+import java.util.Locale;
+
+public class Desafio01 {
+
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault();
+        String idiomaSistema = idioma.getDisplayLanguage(idioma);
+        System.out.println("O idioma do seu sistema esta em: " + idiomaSistema);
+    }
+}
---
+++ Curso Em Video/Desafios/Desafio02.java
@@ -0,0 +1,31 @@
+
+package desafio02;
+
+import java.awt.GraphicsDevice;
+import java.awt.DisplayMode;
+import java.awt.GraphicsEnvironment;
+
+
+public class Desafio02 {
+
+    public static void main(String[] args) {
+        
+        try {
+            GraphicsEnvironment ambienteGrafico = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            
+            GraphicsDevice telaPrincipal = ambienteGrafico.getDefaultScreenDevice();
+            
+            DisplayMode modoExibicao = telaPrincipal.getDisplayMode();
+            
+            int largura = modoExibicao.getWidth();
+            int altura = modoExibicao.getHeight();
+            
+            System.out.println("A resolucao da sua tela principal e: " + largura + "x" + altura);
+            
+        }catch (Exception e) {
+            
+            System.out.println("Ocorreu um erro ao tentar obter a resolução de tela.");
+            e.printStackTrace();
+        }
+    }
+}
